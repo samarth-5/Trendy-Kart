6ad06df0118c0f6e84d6952c62de24736eb27f1c
Samarth Narayan <kayaniv321@gmail.com>
2024-07-18 18:55:04 +0530

    flutter setup completed

---
+++ android/app/src/main/kotlin/com/example/trendy_kart/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.trendy_kart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/pages/onBoarding.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xffecefe8),
+      body: Container(child: Column(children: [],),),
+    );
+  }
+}
