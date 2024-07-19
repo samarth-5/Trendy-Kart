95781e9f25e36d184bbb95bb03f0911b05f0d51b
Samarth Narayan <kayaniv321@gmail.com>
2024-07-19 23:56:34 +0530

    firebase setup completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:trendy_kart/pages/bottomnav.dart';
-import 'package:trendy_kart/pages/login.dart';
-import 'package:trendy_kart/pages/product.dart';
 import 'package:trendy_kart/pages/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
