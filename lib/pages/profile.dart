379f3af76bbe866788aaeed96924651b9bd5c3ff
Samarth Narayan <kayaniv321@gmail.com>
2024-08-01 12:53:31 +0530

    profile pic change functionality implemented

--- lib/pages/profile.dart
+++ lib/pages/profile.dart
@@ -1,4 +1,11 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:random_string/random_string.dart';
+import 'package:trendy_kart/services/shared_pref.dart';
+import 'package:trendy_kart/widgets/support_widget.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 class Profile extends StatefulWidget {
   const Profile({super.key});
@@ -8,8 +15,94 @@ class Profile extends StatefulWidget {
 }
 
 class _ProfileState extends State<Profile> {
+  final ImagePicker _picker = ImagePicker();
+  File? selectedImage;
+  String? name, image, email;
+
+  getSharedPref() async {
+    name = await SharedPreferenceHelper().getUserName();
+    email = await SharedPreferenceHelper().getUserEmail();
+    image = await SharedPreferenceHelper().getUserImage();
+    setState(() {});
+  }
+
+  @override
+  void initState() {
+    getSharedPref();
+    super.initState();
+  }
+
+  Future getImage() async {
+    var image = await _picker.pickImage(source: ImageSource.gallery);
+    selectedImage = File(image!.path);
+    uploadItem();
+    setState(() {});
+  }
+
+  uploadItem() async {
+    if (selectedImage != null) {
+      String addId = randomAlphaNumeric(10);
+      Reference firebaseStorageRef =
+          FirebaseStorage.instance.ref().child("blogImage").child(addId);
+      final UploadTask task = firebaseStorageRef.putFile(selectedImage!);
+      TaskSnapshot snapshot = await task;
+      var downloadUrl = await snapshot.ref.getDownloadURL();
+      await SharedPreferenceHelper().saveUserImage(downloadUrl);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: const Color(0xfff2f2f2),
+      appBar: AppBar(
+        backgroundColor: const Color(0xfff2f2f2),
+        title: Text(
+          "Profile",
+          style: AppWidget.boldTextFieldStyle(),
+        ),
+      ),
+      body: name == null
+          ? const Center(child: CircularProgressIndicator())
+          : Container(
+              child: Column(
+                children: [
+                  selectedImage != null
+                      ? GestureDetector(
+                          onTap: () {
+                            getImage();
+                          },
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.file(
+                                selectedImage!,
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        )
+                      : GestureDetector(
+                          onTap: () {
+                            getImage();
+                          },
+                          child: Center(
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: Image.asset(
+                                "images/profile.png",
+                                height: 150,
+                                width: 150,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                          ),
+                        ),
+                ],
+              ),
+            ),
+    );
   }
 }
